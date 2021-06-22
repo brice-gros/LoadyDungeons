c41bf9d402d7ad4f62cfd48e0ccab0801b39e79a
Brice Gros <brice.gros@unity3d.com>
2021-06-23 01:19:45 +0200

    use navmesh for moving, physics for gravity, and plan collider for picking

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 using UnityEngine.AI;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using UnityEditor;
 
 [RequireComponent(typeof(Rigidbody))]
 public class PlayerController : MonoBehaviour
@@ -22,7 +23,8 @@ public class PlayerController : MonoBehaviour
     [SerializeField]
     private Image m_KeyUI;
 
-    private Rigidbody m_Rigidbody;
+    [SerializeField]
+    private TMPro.TextMeshPro m_DebugText;
 
     private int m_VelocityHash = Animator.StringToHash("Velocity");
 
@@ -30,19 +32,21 @@ public class PlayerController : MonoBehaviour
     
     [SerializeField]
     private GameObject m_level;
-    
+    public float dotProd;
+    public float movMagnitude;
+    private Vector3 m_Velocity;
     private RaycastHit m_PhysHitInfo;
     private NavMeshHit m_NavHitInfo;
-
     private MeshCollider m_MeshCollider;
    
     const float k_MinMovementDistance = 1.2f;
 
     void Start()
     {
-        m_Rigidbody = GetComponent<Rigidbody>();
         m_AnimatorController = GetComponent<Animator>();
         m_MainCamera = Camera.main;
+        
+        // for screen to world raycast, sadly no NavMesh.Raycast3d()...
         var navTriangles = NavMesh.CalculateTriangulation();
         var mesh = new Mesh();
         mesh.SetVertices(navTriangles.vertices);
@@ -89,8 +93,8 @@ public class PlayerController : MonoBehaviour
     private IEnumerator LevelCompleted()
     {
         yield return new WaitForSeconds(2.15f);
-        m_Rigidbody.transform.LookAt(m_MainCamera.transform.position, Vector3.up);
-        m_Rigidbody.velocity = (m_MainCamera.transform.position - m_Rigidbody.transform.position).normalized  * m_MovementSpeed;
+        transform.LookAt(m_MainCamera.transform.position, Vector3.up);
+        m_Velocity = (m_MainCamera.transform.position - transform.position).normalized  * m_MovementSpeed;
         m_AnimatorController.Play("Dino_Victory");
         yield return new WaitForSeconds(3.0f);
         SceneManager.LoadScene(m_NextLevel);
@@ -107,13 +111,27 @@ public class PlayerController : MonoBehaviour
             Touch touch = Input.GetTouch(0);
             MoveToPosition(touch.position);
         }
+        if (m_Velocity != Vector3.zero) {
+            // sadly no MoveOnNavMeshQuery, to smooth movement along the edges, GwNavigation where are you ❤
+            var maxMove = m_NavHitInfo.position - transform.position;
+            var currentMove = m_Velocity * Time.deltaTime;
+            if (maxMove.sqrMagnitude - currentMove.sqrMagnitude < 0.01f) {
+                transform.position = m_NavHitInfo.position;
+                m_Velocity = Vector3.zero;
+            } else {
+                transform.position += currentMove;
+                m_Velocity *= 0.5f;
+                if (m_Velocity.sqrMagnitude < 0.005f) {
+                    m_Velocity = Vector3.zero;
+                }
+            }
+        }
         
         // apply animation
-
         if(m_AnimatorController != null)
-            m_AnimatorController.SetFloat(m_VelocityHash, m_Rigidbody.velocity.magnitude);
+            m_AnimatorController.SetFloat(m_VelocityHash, m_Velocity.magnitude);
 
-        var cameraLookAtPos = m_Rigidbody.transform.position + Vector3.up * 3.0F;
+        var cameraLookAtPos = transform.position + Vector3.up * 3.0F;
         var vecDiff = (cameraLookAtPos - m_MainCamera.transform.position);
         m_MainCamera.transform.LookAt(cameraLookAtPos);
         if (vecDiff.sqrMagnitude < 100.0F) {
@@ -130,26 +148,31 @@ public class PlayerController : MonoBehaviour
         if (Physics.Raycast(m_MainCamera.ScreenPointToRay(screenPosition), out m_PhysHitInfo, Mathf.Infinity, m_InputCollisionLayer))
         {
             // don't move if touching close to character
-            if (Vector3.Distance(m_Rigidbody.position, m_PhysHitInfo.point) > k_MinMovementDistance)
-            {
-                Debug.DrawLine(m_Rigidbody.position, m_PhysHitInfo.point, Color.red, 5);
+            if (Vector3.Distance(transform.position, m_PhysHitInfo.point) > k_MinMovementDistance)
+            {               
+                NavMesh.Raycast(transform.position, m_PhysHitInfo.point, out m_NavHitInfo, NavMesh.AllAreas);
+
                 // calculate move direction vector
-                Vector3 movementDirection = m_PhysHitInfo.point - m_Rigidbody.position;    
+                Vector3 movementDirection = m_PhysHitInfo.point - transform.position;    
+                movementDirection.y = 0.0f;
                 movementDirection.Normalize();
                 var velocity = movementDirection * m_MovementSpeed;
                 
-                if (!NavMesh.Raycast(m_Rigidbody.position, m_PhysHitInfo.point, out m_NavHitInfo, NavMesh.AllAreas)) {
-                    // rotation
-                    m_Rigidbody.transform.LookAt(m_PhysHitInfo.point, Vector3.up);
-                    // lock rotation to y 
-                    Vector3 eulerAngle = m_Rigidbody.transform.eulerAngles;
-                    m_Rigidbody.transform.eulerAngles = new Vector3(0, eulerAngle.y, 0);
-
-                    
-
-                    // apply calculated velocity
-                    m_Rigidbody.velocity = movementDirection * m_MovementSpeed;
+                // sadly, no visual debug replay tool, NavigationLab I miss you ❤
+                var debugDelay = 5;
+                Debug.DrawLine(transform.position, m_PhysHitInfo.point, Color.red, 5);
+                Debug.DrawRay(m_NavHitInfo.position, Vector3.up, Color.magenta, 5);
+                Debug.DrawRay(m_NavHitInfo.position, m_NavHitInfo.normal, Color.yellow, 5);
+                if (m_DebugText) {
+                    // sadly no Debug.Text3d()
+                    m_DebugText.text = $"{m_NavHitInfo.distance}";
                 }
+                // rotation
+                var lookAt = m_PhysHitInfo.point;
+                lookAt.y = transform.position.y;
+                transform.LookAt(lookAt, Vector3.up);
+                // apply calculated velocity
+                m_Velocity = velocity;
             }
         }
     }
